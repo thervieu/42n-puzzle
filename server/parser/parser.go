27ae467232277e383683a42e10a80ba85bf21a0e
thervieu <thibault99hervieu@gmail.com>
2023-01-20 14:17:47 +0100

    fix(server): added school puzzle generator, fixed file parser with size reader and utils functions

--- server/parser/parser.go
+++ server/parser/parser.go
@@ -57,6 +57,15 @@ func parseArgs(args []string) (search string, heuristic string, fileName string)
 	}
 	return
 }
+func isStringNumerical(word string) (bool) {
+
+	for _, c := range word {
+		if (string(c) < "0" || string(c) > "9") {
+			return false
+		}
+	}
+	return true
+}
 
 func contains(arr []int, number int) (bool) {
 	for _, nb := range arr {
@@ -67,8 +76,59 @@ func contains(arr []int, number int) (bool) {
 	return false
 }
 
+func parseSize(strSplitted []string) (int, error) {
 
-func parseFile(fileName string) ([]int, error) {
+	if (len(strSplitted) > 1) { // too many arguments
+		return 0, errors.New("size has too many arguments")
+	}
+
+	number, err := strconv.Atoi(strSplitted[0])
+	if err != nil {
+		return 0,  errors.New(fmt.Sprintf("err in strconv.Atoi : %T \n %v\n", number, number))
+	}
+	if (number < 3) {
+		return 0,  errors.New("only puzzles of size striclty superior to 2 are considered")
+	}
+
+	return number, nil
+}
+
+func concatStrArr(strSplitted []string) (str string) {
+	str = ""
+	for _, word := range strSplitted {
+		str += word
+	}
+	return str
+}
+
+func parsePuzzleArray(size int, p []int, strSplitted []string) ([]int, error) {
+
+	if (size != len(strSplitted)) {
+		return []int{}, errors.New("line: " + concatStrArr(strSplitted) + " size ("  + strconv.Itoa(size) + ") differs from number of integers (" + strconv.Itoa(len(strSplitted)) + ")")
+	}
+	for _, word := range strSplitted {
+		if (isStringNumerical(word) == false) {
+			return []int{}, errors.New("word: " + word + " is not numeral")
+		}
+	
+		number, err := strconv.Atoi(word)
+		if err != nil {
+			return []int{}, errors.New(fmt.Sprintf("err in strconv.Atoi : %T \n %v\n", number, number))
+		}
+	
+		if (number >= size * size) {
+			return []int{}, errors.New("number " + strconv.Itoa(number)  + " is too big for a " + strconv.Itoa(size) + "-puzzle" )
+		}
+		if (contains(p, number)) { // number already exists
+			return []int{}, errors.New("number " + strconv.Itoa(number) + " already in array")
+		}
+	
+		p = append(p, number)
+	}
+	return p, nil
+}
+
+func parseFile(fileName string) (uint8, []int, error) {
 	readFile, err := os.Open(fileName)
 
 	if err != nil {
@@ -88,42 +148,36 @@ func parseFile(fileName string) ([]int, error) {
 			continue
 		}
 		str = strings.Split(str, "#")[0] // line without comment
-		strSplitted := strings.Fields(str) //
-
-		if (len(strSplitted) <= 2) {
-			return []int{}, errors.New("line: " + str + " has two numbers or less")
-		}
+		strSplitted := strings.Fields(str)
+		
 
 		if (size == 0) { // set size on first
-			size = len(strSplitted)
-		} else if (size != len(strSplitted)) {
-			return []int{}, errors.New("line: " + str + " does not have the right size")
-		}
-
-		for _, word := range strSplitted {
-			for _, c := range word {
-				if (string(c) < "0" || string(c) > "9") {
-					return []int{}, errors.New("word: " + word + " is not numeral")
-				}
+			if (len(strSplitted) == 0) {
+				continue
 			}
-			number, err := strconv.Atoi(word)
+
+			number, err := parseSize(strSplitted)
 			if err != nil {
-				fmt.Printf("err in strconv.Atoi : %T \n %v\n", number, number)
+				readFile.Close()
+				return 0, []int{}, err
 			}
-
-			if (number >= size * size) {
-				return []int{}, errors.New("number " + strconv.Itoa(number)  + " is too big for a " + strconv.Itoa(size) + "-puzzle" )
-
-			}
-			if (contains(puzzleArray, number)) {
-				return []int{}, errors.New("number " + strconv.Itoa(number) + " already in array")
-			}
-			puzzleArray = append(puzzleArray, number)
+			size = number
+			continue
 		}
 		
+		arr, err := parsePuzzleArray(size, puzzleArray, strSplitted)
+		if (err != nil) {
+			readFile.Close()
+			return 0, []int{}, err
+		}
+		puzzleArray = append(puzzleArray, arr...)
+	}
+
+	if (size * size != len(puzzleArray)) {
+		readFile.Close()
+		return 0, []int{}, errors.New("erro: file " + fileName + ": has " + strconv.Itoa(len(puzzleArray)) + " numbers, need " + strconv.Itoa(size * size))
 	}
 
 	readFile.Close()
-
-	return puzzleArray, nil
+	return uint8(size), puzzleArray, nil
 }
