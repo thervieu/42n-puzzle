c1a0928caa9bef0e99fd4ab002363d444670e8b9
thervieu <thibault99hervieu@gmail.com>
2023-01-20 12:51:41 +0100

    feat(server): standalone: file parser

--- server/parser/parser.go
+++ server/parser/parser.go
@@ -1,9 +1,12 @@
 package parser
 
 import (
-	"fmt"
 	"os"
+	"fmt"
+	"bufio"
+	"errors"
 	"strings"
+	"strconv"
 )
 
 func help() {
@@ -23,25 +26,29 @@ func parseArgs(args []string) (search string, heuristic string, fileName string)
 		if arg == "-h" || arg == "--help" {
 			help() // prints and quits
 		}
-		if strings.Contains(arg, "=") == false ||
-			len(strings.Split(arg, "=") != 2) {
+		if (strings.Contains(arg, "=") == false ||
+			len(strings.Split(arg, "=")) != 2) {
 
 			wrongArg(arg)
 		}
-		if strings.hasPrefix(arg, "--search=") &&
+		if (strings.HasPrefix(arg, "--search=") &&
+			len(strings.Split(arg, "=")) == 2 &&
 			(strings.Split(arg, "=")[1] == "open" ||
-				strings.Split(arg, "=")[1] == "greedy") {
+				strings.Split(arg, "=")[1] == "greedy")) {
 
 			search = strings.Split(arg, "=")[1]
 
-		} else if strings.hasPrefix(arg, "--heuristic=") &&
+		} else if (strings.HasPrefix(arg, "--heuristic=") &&
+			len(strings.Split(arg, "=")) == 2 &&
 			(strings.Split(arg, "=")[1] == "euclidean" ||
-				strings.Split(arg, "=")[1] == "manhattan" ||
-				strings.Split(arg, "=")[1] == "hamming") {
+			strings.Split(arg, "=")[1] == "manhattan" ||
+			strings.Split(arg, "=")[1] == "hamming")) {
 
 			heuristic = strings.Split(arg, "=")[1]
-		} else if strings.hasPrefix(arg, "--filename=" &&
-			strings.hasSuffix(arg, ".txt")) {
+		} else if (strings.HasPrefix(arg, "--filename=") && 
+			len(strings.Split(arg, "=")) == 2 &&
+			len(strings.Split(strings.Split(arg, "=")[1], ".")) == 2 &&
+			strings.HasSuffix(arg, ".txt")) {
 			fileName = strings.Split(arg, "=")[1]
 		} else {
 			wrongArg(arg)
@@ -49,4 +56,74 @@ func parseArgs(args []string) (search string, heuristic string, fileName string)
 
 	}
 	return
+}
+
+func contains(arr []int, number int) (bool) {
+	for _, nb := range arr {
+		if (nb == number) {
+			return true
+		}
+	}
+	return false
+}
+
+
+func parseFile(fileName string) ([]int, error) {
+	readFile, err := os.Open(fileName)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	size := 0
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	
+	puzzleArray := []int{}
+	for fileScanner.Scan() {
+		str := fileScanner.Text()
+
+		if (strings.HasPrefix(str, "#")) { // if comment line
+			continue
+		}
+		str = strings.Split(str, "#")[0] // line without comment
+		strSplitted := strings.Fields(str) //
+
+		if (len(strSplitted) <= 2) {
+			return []int{}, errors.New("line: " + str + " has two numbers or less")
+		}
+
+		if (size == 0) { // set size on first
+			size = len(strSplitted)
+		} else if (size != len(strSplitted)) {
+			return []int{}, errors.New("line: " + str + " does not have the right size")
+		}
+
+		for _, word := range strSplitted {
+			for _, c := range word {
+				if (string(c) < "0" || string(c) > "9") {
+					return []int{}, errors.New("word: " + word + " is not numeral")
+				}
+			}
+			number, err := strconv.Atoi(word)
+			if err != nil {
+				fmt.Printf("err in strconv.Atoi : %T \n %v\n", number, number)
+			}
+
+			if (number >= size * size) {
+				return []int{}, errors.New("number " + strconv.Itoa(number)  + " is too big for a " + strconv.Itoa(size) + "-puzzle" )
+
+			}
+			if (contains(puzzleArray, number)) {
+				return []int{}, errors.New("number " + strconv.Itoa(number) + " already in array")
+			}
+			puzzleArray = append(puzzleArray, number)
+		}
+		
+	}
+
+	readFile.Close()
+
+	return puzzleArray, nil
 }
