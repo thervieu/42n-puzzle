2fcccbfc92836350a0c4446d97a998d0c069b0f4
anroche <anroche@student.42.fr>
2023-01-28 18:46:53 +0100

    front : structure page

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+	import Range from "../Components/Range.svelte"	
+   
+   	let selected = '20';
+	
+	function onChange(event) {
+		selected = event.currentTarget.value;
+	}
+</script>
+
+<h1>N-Puzzle</h1>
+<h3>Size</h3>
+<Range />
+<h3>Search</h3>
+<label>
+	<input checked={selected===1} on:change={onChange} type="radio" name="amount" value="1" /> 10
+</label>
+<label>
+	<input checked={selected===2} on:change={onChange} type="radio" name="amount" value="2" /> 10
+</label>
+<h3>Heuristic</h3>
+<label>
+	<input checked={selected===3} on:change={onChange} type="radio" name="amount" value="3" /> 10
+</label>
+<label>
+	<input checked={selected===4} on:change={onChange} type="radio" name="amount" value="4" /> 10
+</label>
+<label>
+	<input checked={selected===5} on:change={onChange} type="radio" name="amount" value="5" /> 10
+</label>
+<button>Solve</button>
