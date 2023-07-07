874b137b9bfa6997acab33c4cf2d85f2c43fd026
anroche <anroche@student.42.fr>
2023-07-07 16:31:17 +0200

    split Puzzle / fix centering

--- client/src/Components/02-Molecules/Board.svelte
+++ client/src/Components/02-Molecules/Board.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
 	import Cell from '../01-Atoms/Cell.svelte';
 	export let board: number[][];
 	export let moveCallback: (row: number, col: number) => void;
 	export let clickable: boolean;
 	export let imagePaths: string[] | null;
-
-
 </script>
 
 <div class="board" style:--size={board.length}>
@@ -16,20 +12,18 @@
 			{#if imagePaths !== null}
 				<Cell size={board.length} {i} {j} {moveCallback} {cell} {clickable} image={imagePaths} />
 			{:else}
-				<Cell size={board.length} {i} {j} {moveCallback} {cell} {clickable} image={null}/>
+				<Cell size={board.length} {i} {j} {moveCallback} {cell} {clickable} image={null} />
 			{/if}
 		{/each}
 	{/each}
 </div>
 
 <style>
-	img {
-		max-width: 100%;
-		height: auto;
-	}
 	.board {
 		display: grid;
 		grid-template-columns: repeat(var(--size), 1fr);
 		grid-template-rows: repeat(var(--size), 1fr);
+		width: 240px;
+		margin: auto;
 	}
 </style>
---
+++ client/src/Components/02-Molecules/InputsRow.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let inputs: { label: string; value: number }[];
+	export let checkedValue: number;
+	export let handleChange: (e: any) => void;
+</script>
+
+<div>
+	{#each inputs as input}
+		<label>
+			<input
+				type="radio"
+				name="graphed"
+				value={input.value}
+				checked={checkedValue === input.value}
+				on:change={handleChange}
+			/>
+			{input.label}
+		</label>
+	{/each}
+</div>
